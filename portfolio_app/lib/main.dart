77537d4d97930ac0023e94ad73d8cde2d44b966e
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-06-17 11:05:31 +0545

    Image as background
    Used Image as background and image fonts and icons colors to white.

--- portfolio_app/lib/main.dart
+++ portfolio_app/lib/main.dart
@@ -14,91 +14,137 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 100, left: 22),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                const CircleAvatar(
-                  radius: 42,
-                  backgroundImage: AssetImage('assets/images/profile.jpg'),
-                ),
-                const SizedBox(width: 51),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Jack',
-                      style: TextStyle(
-                          fontSize: 31,
-                          fontFamily: 'assets/fonts/Roboto-Regular.ttf'),
-                    ),
-                    Text(
-                      'Flutter App Developer',
-                      style: TextStyle(fontSize: 19),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            const SizedBox(height: 29),
-            Padding(
-              padding: const EdgeInsets.only(left: 31),
-              child: Column(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100, left: 22),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  Row(
-                    children: const [
-                      Icon(Icons.school_outlined, size: 41),
-                      SizedBox(width: 26),
-                      Text('School Name', style: TextStyle(fontSize: 26)),
-                    ],
+                  const CircleAvatar(
+                    radius: 42,
+                    backgroundImage: AssetImage('assets/images/profile.jpg'),
                   ),
-                  const SizedBox(height: 10),
-                  Row(
+                  const SizedBox(width: 51),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
-                      Icon(Icons.computer_outlined, size: 41),
-                      SizedBox(width: 26),
-                      Text('School Name', style: TextStyle(fontSize: 26)),
+                      Text(
+                        'Jack Rai',
+                        style: TextStyle(
+                            fontSize: 28,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'assets/fonts/Roboto-Regular.ttf'),
+                      ),
+                      Text(
+                        'Flutter App Developer',
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.white,
+                        ),
+                      ),
                     ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: const [
-                      Icon(Icons.location_on_outlined, size: 41),
-                      SizedBox(width: 26),
-                      Text('School Name', style: TextStyle(fontSize: 26)),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: const [
-                      Icon(Icons.email_outlined, size: 41),
-                      SizedBox(width: 26),
-                      Text('School Name', style: TextStyle(fontSize: 26)),
-                    ],
-                  ),
-                  Row(
-                    children: const [
-                      Icon(Icons.phone_android_outlined, size: 41),
-                      SizedBox(width: 26),
-                      Text('School Name', style: TextStyle(fontSize: 26)),
-                    ],
-                  ),
+                  )
                 ],
               ),
-            ),
-            const SizedBox(height: 51),
-            const Padding(
-              padding: EdgeInsets.all(19.0),
-              child: Text(
-                'Hello my name is Jack and this is my portfolio app.',
-                style: TextStyle(fontSize: 23),
+              const SizedBox(height: 29),
+              Padding(
+                padding: const EdgeInsets.only(left: 31),
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(Icons.school_outlined,
+                            size: 41, color: Colors.white),
+                        SizedBox(width: 26),
+                        Text(
+                          'School Name',
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: const [
+                        Icon(Icons.computer_outlined,
+                            size: 41, color: Colors.white),
+                        SizedBox(width: 26),
+                        Text(
+                          'School Name',
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: const [
+                        Icon(Icons.location_on_outlined,
+                            size: 41, color: Colors.white),
+                        SizedBox(width: 26),
+                        Text(
+                          'School Name',
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: const [
+                        Icon(Icons.email_outlined,
+                            size: 41, color: Colors.white),
+                        SizedBox(width: 26),
+                        Text(
+                          'School Name',
+                          style: TextStyle(fontSize: 26, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: const [
+                        Icon(Icons.phone_android_outlined,
+                            size: 41, color: Colors.white),
+                        SizedBox(width: 26),
+                        Text(
+                          'School Name',
+                          style: TextStyle(fontSize: 26, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 19),
-            const Text('Made By Prem'),
-          ],
+              const SizedBox(height: 51),
+              const Padding(
+                padding: EdgeInsets.all(19.0),
+                child: Text(
+                  'Hello my name is Jack and this is my portfolio app.',
+                  style: TextStyle(fontSize: 21, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 60),
+              const Text(
+                'Made By Prem',
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
