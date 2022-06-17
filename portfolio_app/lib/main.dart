65c97ed06559aa0903f4bc1e63a4a9dd14e3d41d
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-06-17 10:06:07 +0545

    Added Icon and Text and TextStyle
    Icon and text with style is added.

--- portfolio_app/lib/main.dart
+++ portfolio_app/lib/main.dart
@@ -20,19 +20,73 @@ class HomeScreen extends StatelessWidget {
           children: [
             Row(
               children: [
-                CircleAvatar(
+               const CircleAvatar(
                   radius: 42,
-                  backgroundImage:
-                      NetworkImage('https://picsum.photos/200/300'),
+                  backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                  ),
                 ),
+                const SizedBox(width: 51),
                 Column(
-                  children: [
-                    Text('Name'),
-                    Text('Description'),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const[
+                    Text(
+                      'Jack',
+                      style: TextStyle(fontSize: 31),
+                    ),
+                    Text(
+                      'Description',
+                      style: TextStyle(fontSize: 19),
+                    ),
                   ],
                 )
               ],
-            )
+            ),
+           const SizedBox(height: 51),
+            Padding(
+              padding: const EdgeInsets.only(left: 31),
+              child: Column(
+                children: [
+                  Row(
+                    children: const [
+                      Icon(Icons.school_outlined,size: 41),
+                      SizedBox(width: 21),
+                      Text('School Name', style: TextStyle(fontSize: 21)),
+                    ],
+                  ),
+                  Row(
+                    children:const [
+                      Icon(Icons.computer_outlined,size: 41),
+                      SizedBox(width: 21),
+                      Text('School Name', style: TextStyle(fontSize: 21)),
+                    ],
+                  ),
+                  Row(
+                    children: const [
+                      Icon(Icons.location_on_outlined,size: 41),
+                      SizedBox(width: 21),
+                      Text('School Name', style: TextStyle(fontSize: 21)),
+                    ],
+                  ),
+                  Row(
+                    children: const[
+                      Icon(Icons.email_outlined,size: 41),
+                      SizedBox(width: 21),
+                      Text('School Name', style: TextStyle(fontSize: 21)),
+                    ],
+                  ),
+                  Row(
+                    children: const[
+                      Icon(Icons.phone_android_outlined,size: 41),
+                      SizedBox(width: 21),
+                      Text('School Name', style: TextStyle(fontSize: 21)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+           const Text('About Me'),
+           const Text('Made By Prem'),
           ],
         ),
       ),
